b63a821efdadb53f44f4c56cd2ef3a3df605e419
Patryk Barchwic <barchwicp@gmail.com>
2022-02-01 19:13:13 +0100

    Added Photo component

---
+++ src/app/component/photo/photo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'photo',
+  templateUrl: 'photo.component.html',
+  styleUrls: ['./photo.component.scss'],
+})
+export class PhotoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
